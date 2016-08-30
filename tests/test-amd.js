c9a72d62c179aa7896a70490e4444343d18908ec
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-08-30 20:55:11 +0200

    Uodated test code for AMD

--- tests/test-amd.js
+++ tests/test-amd.js
@@ -10,10 +10,10 @@ define(['memorystorage'], function(MemoryStorage){
 		assert.ok(typeof MemoryStorage === 'function', 'MemoryStorage is a function');
 		assert.ok(window.MemoryStorage === undefined, 'global MemoryStorage is NOT defined');
 	});
-	
+
 	QUnit.test("W3C Web Storage API Compliance Test", function( assert ) {
 		var store = new MemoryStorage('local');
-	  
+
 		store.clear();
 		assert.ok(store.length===0, "store cleared");
 		store.setItem('test0', 'data0');
@@ -28,7 +28,7 @@ define(['memorystorage'], function(MemoryStorage){
 		assert.ok(store.length===2, 'value added correctly with index operators');
 		store.setItem('test2', 'data2');
 		assert.ok(store.length===3, 'three items added to store');
-		assert.ok(Object.keys(store).length == (7+3), "store has 10 enumerable properties (id, 6 api methods + 3 stored items)");
+		assert.ok(Object.keys(store).length == (3), "store has 3 enumerable properties (no api methods + 3 stored items)");
 		assert.ok(store.getItem('test1')==='data1' && store.getItem('test2')==='data2', "retrieved values matches stored values");
 		var keyOrderBefore = '';
 		for (var i=0; i<store.length; i++) {
@@ -45,28 +45,28 @@ define(['memorystorage'], function(MemoryStorage){
 		assert.ok(store.length===2, "item removed correctly with removeItem");
 		store.removeItem('test1');
 		assert.ok(store.length===2, "double removal has no effect");
-		assert.ok(store.getItem('test1')===undefined, "get removed item returns undefined");
+		assert.ok(store.getItem('test1')===null, "get removed item returns null");
 
 		store.setItem('getItem', 'test');
 		assert.ok(typeof store.getItem === 'function', "store API methods cannot be overwritten.");
 		assert.ok(store.getItem('getItem') === 'test', "getItem successfully retrieves item with API name.");
 		store.removeItem('getItem');
-		assert.ok(store.getItem('getItem') === undefined, "After removal of item with API name, getItem returns undefined.");
-		
+		assert.ok(store.getItem('getItem') === null, "After removal of item with API name, getItem returns null.");
+
 		var glob = new MemoryStorage();
 		assert.ok(glob.length===0, "local store items are not visible globally");
 		glob.setItem('glob0', 'data0');
 		assert.ok(glob.length===1 && glob.getItem('glob0')==='data0', "globally stored items are retrieved ok");
-		assert.ok(store.getItem('glob0')===undefined, "global items are not visible in the local store");
+		assert.ok(store.getItem('glob0')===null, "global items are not visible in the local store");
 		glob.removeItem('glob0');
 		assert.ok(glob.length===0, "global length is updated correctly");
 		assert.ok(glob.key(0)===null, "global keys are removed correctly");
-		assert.ok(glob.getItem('glob0')===undefined, "global values are removed correctly");
+		assert.ok(glob.getItem('glob0')===null, "global values are removed correctly");
 
 		store.clear();
 		assert.ok(store.length===0, "store is cleared");
 		assert.ok(store.key(0)===null, "no keys in cleared store");
-		assert.ok(store.getItem('test0')===undefined, "no values in cleared store");
+		assert.ok(store.getItem('test0')===null, "no values in cleared store");
 	});
 
 	QUnit.test("Multiple Instances Test", function( assert ) {
@@ -103,7 +103,7 @@ define(['memorystorage'], function(MemoryStorage){
 		var store1 = MemoryStorage('local');
 		assert.ok(store1, 'Existing store is defined and not null');
 		assert.ok(store1 instanceof MemoryStorage, 'Existing store is instanceof MemoryStorage');
-		
+
 		var store2 = MemoryStorage('new-store');
 		assert.ok(store2, 'New store is defined and not null');
 		assert.ok(store2 instanceof MemoryStorage, 'New store is instanceof MemoryStorage');
